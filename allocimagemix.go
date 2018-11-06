73b851bda8c68bccb8e43c03f026c8fa7958c29b
Fazlul Shahriar <fshahriar@gmail.com>
2018-11-06 01:44:24 -0500

    Add AllocImageMix method to Display
    This is for Edwood.
    Forgot to add this file in my last PR.

---
+++ allocimagemix.go
@@ -0,0 +1,25 @@
+package draw
+
+import "image"
+
+// AllocImageMix blends the two colors to create a tiled image representing
+// their combination. For pixel formats of 8 bits or less, it creates a 2x2
+// pixel texture whose average value is the mix. Otherwise it creates a 1-pixel
+// solid color blended using 50% alpha for each.
+func (d *Display) AllocImageMix(color1, color3 Color) *Image {
+	//	if d.ScreenImage.Depth <= 8 { // create a 2x2 texture
+	//		t, _ := d.allocImage(image.Rect(0, 0, 1, 1), d.ScreenImage.Pix, false, color1)
+	//		b, _ := d.allocImage(image.Rect(0, 0, 2, 2), d.ScreenImage.Pix, true, color3)
+	//		b.draw(image.Rect(0, 0, 1, 1), t, nil, image.ZP)
+	//		t.free()
+	//		return b
+	//	}
+
+	// use a solid color, blended using alpha
+	qmask, _ := d.AllocImage(image.Rect(0, 0, 1, 1), GREY8, true, 0x3F3F3FFF)
+	t, _ := d.AllocImage(image.Rect(0, 0, 1, 1), d.ScreenImage.Pix, true, color1)
+	b, _ := d.AllocImage(image.Rect(0, 0, 1, 1), d.ScreenImage.Pix, true, color3)
+	b.Draw(b.R, t, qmask, image.ZP)
+	t.Free()
+	return b
+}
